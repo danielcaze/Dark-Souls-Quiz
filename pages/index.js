b20210f67bafefd4b27664f8624e0657f0b7417e
Darrow <nicedarrow12@gmail.com>
2021-01-28 16:53:21 -0300

    Apenas o básico

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,16 @@ import styled from 'styled-components'
 
 const Title = styled.h1`
   font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.secondary};
 `
+
+//function Title(props) { // propcidades/propriedas
+//  return (
+//    <h1>
+//      {props.children}
+//    </h1>
+//  )
+//}
 
 export default function Home() {
   return <Title>My page</Title>
